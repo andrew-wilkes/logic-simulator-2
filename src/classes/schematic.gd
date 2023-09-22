0566f2fc116b3e7272bc96e87cccb7fabe1be266
andrew-wilkes <github@urgentclick.com>
2023-09-22 13:59:04 +0900

    Improve selection code

--- src/classes/schematic.gd
+++ src/classes/schematic.gd
@@ -2,11 +2,10 @@ class_name Schematic
 
 extends GraphEdit
 
-const PART_OFFSET_STEP = Vector2(100, 100)
 const PART_INITIAL_OFFSET = Vector2(100, 100)
 
 var circuit: Circuit
-var selected_parts = {}
+var selected_parts = []
 var part_name_id = 0
 
 func _ready():
@@ -14,9 +13,9 @@ func _ready():
 	node_deselected.connect(deselect_part)
 	node_selected.connect(select_part)
 	delete_nodes_request.connect(delete_selected_parts)
-	copy_nodes_request.connect(copy_selected_parts)
 	connection_request.connect(connect_wire)
 	disconnection_request.connect(disconnect_wire)
+	duplicate_nodes_request.connect(duplicate_selected_parts)
 
 
 func connect_wire(from_part, from_pin, to_part, to_pin):
@@ -47,40 +46,41 @@ func clear():
 
 
 func select_part(part):
-	selected_parts[part] = true
+	selected_parts.append(part)
 
 
 func deselect_part(part):
-	selected_parts[part] = false
+	selected_parts.erase(part)
 
 
 func delete_selected_parts(_arr):
 	# _arr only lists parts with a close button
-	for part in selected_parts.keys():
-		if selected_parts[part]:
-			delete_selected_part(part)
+	for part in selected_parts:
+		delete_selected_part(part)
 	selected_parts.clear()
 	circuit.connections = get_connection_list()
 
 
 func delete_selected_part(part):
-	circuit.parts.erase(part)
 	for con in get_connection_list():
 		if con.to == part.name or con.from == part.name:
 			disconnect_node(con.from, con.from_port, con.to, con.to_port)
 	part.queue_free()
 
 
-func copy_selected_parts():
-	for part in selected_parts.keys():
-		if selected_parts[part]:
-			for node in get_children():
-				if node == part:
-					var new_part = part.duplicate()
-					new_part.position_offset += PART_OFFSET_STEP
-					part_name_id += 1
-					new_part.name = "part" + str(part_name_id)
-					add_child(new_part)
+func duplicate_selected_parts():
+	var offset = get_local_mouse_position()
+	var first_part = true
+	for part in selected_parts:
+		if first_part:
+			first_part = false
+			offset = abs(offset) - part.position_offset
+		var new_part = part.duplicate()
+		new_part.selected = false
+		new_part.position_offset += offset
+		part_name_id += 1
+		new_part.name = "part" + str(part_name_id)
+		add_child(new_part)
 
 
 func add_part(part: Part):
--- src/main.gd
+++ src/main.gd
@@ -7,6 +7,5 @@ func _ready():
 
 func _on_button_pressed():
 	var part = Part.new()
-	part.position_offset = Vector2(100, 100) + $VB/Schematic.scroll_offset / $VB/Schematic.zoom
 	part.title = "Test"
 	$VB/Schematic.add_part(part)
