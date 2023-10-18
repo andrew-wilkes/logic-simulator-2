35dcd249d53e30dab4581f79c59c91ac8a0911de
andrew-wilkes <github@urgentclick.com>
2023-10-18 17:52:06 +0900

    Small update to keyboard part

--- src/parts/Keyboard.gd
+++ src/parts/Keyboard.gd
@@ -11,8 +11,10 @@ func _init():
 
 func _on_chars_text_changed(new_text):
 	var length = new_text.length()
-	if length > 0 and length > last_text_length:
+	if length > last_text_length:
+		# Output the character code of the last key that was typed by the user
 		var char_code = new_text.right(1).to_ascii_buffer()[0]
-		print(char_code)
+		if DEBUG:
+			print(char_code)
 		update_output_value(RIGHT, 0, char_code)
 	last_text_length = length
