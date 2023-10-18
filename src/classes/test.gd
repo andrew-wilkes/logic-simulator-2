d7f93614f0a2c7e0c061a0c8f1d86c0cde4f430b
andrew-wilkes <github@urgentclick.com>
2023-10-18 14:22:59 +0900

    Add test code

--- src/classes/test.gd
+++ src/classes/test.gd
@@ -47,17 +47,23 @@ func run_tests(spec: String, inputs, outputs):
 	var start = spec.find("output-list")
 	var end = spec.find(";", start)
 	var output_format = spec.substr(start + 12, end - start - 12)
-	var tokens = output_format.split(" ", false)
 	
+	# Get the output pin names of interest
+	var tokens = output_format.split(" ", false)
 	var output_pins = []
+	var output_formats = []
 	for token in tokens:
-		output_pins.append(token.split("%")[0])
+		var pf = token.split("%") # [pin_name, format]
+		output_pins.append(pf[0])
+		output_formats.append(pf[1])
+	
+	var output = get_output_header(output_pins, output_formats)
 
-	# Get the tests
+	# Get the test specifications
 	var tests = []
 	while true:
 		start = spec.find("set", end)
-		if start < 0:
+		if start < 0: # Ends the parsing while loop
 			break
 		end = spec.find(";", start)
 		if end < 0: # This should not occur
@@ -65,29 +71,89 @@ func run_tests(spec: String, inputs, outputs):
 		tests.append(spec.substr(start, end - start).replace(" ", "").replace("\n", ""))
 	
 	# Run the tests
-	var output_values = []
+	var results = []
 	for test in tests:
+		var pin_values = []
 		var steps = test.split(",", false)
 		for step in steps:
 			tokens = step.split(" ", false)
 			if tokens.size() > 0:
 				match tokens[0]:
 					"set":
-						if tokens.size() == 3:
-							if inputs.has(tokens[1]): # pin name
-								var target = inputs[tokens[1]] # [part, port, port_type]
-								if target[2] == 0:
-									target[0].update_input_level(0, target[1], tokens[2] == "1")
-								else:
-									target[0].update_bus_input_value(0, target[1], int(tokens[2]))
+						# Apply input levels and values to the parts
+						tokens.resize(3)
+						var pin_name = tokens[1]
+						var value = int(tokens[2])
+						pin_values.append(value)
+						if inputs.has(pin_name):
+							var target = inputs[pin_name] # [part, port, port_type]
+							if target[2] == 0: # Wire
+								target[0].update_input_level(0, target[1], value == "1")
+							else: #Bus
+								target[0].update_bus_input_value(0, target[1], value)
 					"eval":
-						# Get the required outputs
+						# Get the required outputs from the parts
 						for pin in output_pins:
 							if outputs.has(pin):
 								var target = outputs[pin]
-								var pin_key = [1, target[1]]
-								output_values.append(int(target[0].pins.get(pin_key, 0)))
+								var pin_key = [1, target[1]] # [side, port]
+								# Cast bools to int and set the value to 0 if the pin has not been touched
+								pin_values.append(int(target[0].pins.get(pin_key, 0)))
 							else:
-								output_values.append(0)
+								# Even if the pin is not found, add zero as the result value
+								pin_values.append(0)
+						results.append(pin_values)
 					"output":
-						pass # Add the output text to the result
+						get_output_results(output, results, output_formats)
+	return output
+
+
+func get_output_header(output_pins, output_formats):
+	# Form the top line like: |   a   |   b   | out |
+	var output = "|"
+	for idx in output_pins.size():
+		var widths = output_formats[idx].right(-1).split(".")
+		widths.resize(3) # There should be 3 values: PadL.Length.PadR
+		# Center align the pin name
+		var min_length = int(widths[0]) + int(widths[1]) + int(widths[2])
+		var pin_name = output_pins[idx]
+		pin_name = pin_name.lpad((min_length + pin_name.length()) / 2, " ")
+		output += pin_name.rpad(min_length, " ") + "|"
+	return output + "\n"
+
+
+func get_output_results(output, results, output_formats):
+	# Add the result lines to output
+	for result in results:
+		output += "|"
+		for idx in output_formats.size():
+			# Format string: (S|B|D|X)PadL.Length.PadR
+			var format = output_formats[idx]
+			if format.length() == 0:
+				format = "B1.1.1" # Default value
+			var widths = format.right(-1).split(".")
+			widths.resize(3) # There should be 3 values: PadL.Length.PadR
+			var value = format_value(int(result[idx]), format[0], int(widths[1]))
+			output += value.lpad(int(widths[0]) + value.length()) + " ".repeat(int(widths[2])) + "|"
+		output += "\n"
+	return output
+
+
+func format_value(value, format, width):
+	match format:
+		"S", "D":
+			value = str(value).rpad(width, " ")
+		"X":
+			format =  "%0" + str(width) + "X"
+			value = format % [value]
+		"B":
+			var bits = PackedStringArray()
+			for n in width:
+				if value % 2:
+					bits.append("1")
+				else:
+					bits.append("0")
+				value /= 2
+			bits.reverse()
+			value = "".join(bits)
+	return value
--- src/test_tests/classes/test_test.gd
+++ src/test_tests/classes/test_test.gd
@@ -10,7 +10,7 @@ const __source = 'res://classes/test.gd'
 
 func test_run_tests() -> void:
 	var test = Test.new()
-	var spec = "// This file is part of www.nand2tetris.org
+	var _spec = "// This file is part of www.nand2tetris.org
 // and the book \"The Elements of Computing Systems\"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/01/And.tst
@@ -40,6 +40,31 @@ set b 1,
 eval,
 output;
 "
-	test.run_tests(spec)
-	# remove this line and complete your test
+	#test.run_tests(spec, [], [])
 	assert_not_yet_implemented()
+	test.free()
+
+
+func test_format_value() -> void:
+	var test = Test.new()
+	assert_str(test.format_value(10, "S", 4)).is_equal("10  ")
+	assert_str(test.format_value(10, "D", 3)).is_equal("10 ")
+	assert_str(test.format_value(10, "X", 4)).is_equal("000A")
+	assert_str(test.format_value(21, "B", 8)).is_equal("00010101")
+	test.free()
+
+
+func test_get_output_header() -> void:
+	var test = Test.new()
+	var output_pins = ["a", "b"]
+	var output_formats = ["B1.3.1", "D2.4.3"]
+	assert_str(test.get_output_header(output_pins, output_formats)).is_equal("|  a  |    b    |\n")
+	test.free()
+
+
+func test_get_output_results() -> void:
+	var test = Test.new()
+	var results = [[1,10,3,0]]
+	var output_formats = ["D1.3.1", "X1.1.1", "B1.4.1", ""]
+	assert_str(test.get_output_results("", results, output_formats)).is_equal("| 1   | A | 0011 | 0 |\n")
+	test.free()
