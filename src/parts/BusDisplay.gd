418fc7088606c31744e08cc179275cc2a94142a7
andrew-wilkes <github@urgentclick.com>
2023-10-31 11:05:39 +0900

    Complete the Display part

---
+++ src/parts/BusDisplay.gd
@@ -0,0 +1,3 @@
+class_name BusDisplay
+
+extends Display
---
+++ src/parts/Display.gd
@@ -0,0 +1,49 @@
+class_name Display
+
+extends Part
+
+func _init():
+	order = 0
+	category = UTILITY
+	@warning_ignore("integer_division")
+	data = {
+		mode = 0,
+		color_index = 1 + 128 / 3
+	}
+
+
+func _ready():
+	super()
+	set_color(data.color_index)
+	%Hue.set_value_no_signal(data.color_index)
+
+
+func evaluate_output_level(_side, port, level):
+	%Segments.material.set_shader_parameter(["a","b","c","d","e","f","g","dp"][port], level)
+
+
+func _on_hue_value_changed(value):
+	set_color(int(value))
+	changed()
+
+
+func set_color(color_index):
+	# There are 129 steps of color_index
+	# 0 sets black bg and white segments
+	# 1 .. 128 sets black bg and colored segments based on hue value
+	# 129 sets white bg and black segments
+	var color
+	var bg_color
+	match color_index:
+		0:
+			color = Color.WHITE
+			bg_color = Color.BLACK
+		129:
+			color = Color.BLACK
+			bg_color = Color.WHITE
+		_:
+			color = Color.from_hsv((color_index - 1) / 128.0, 1.0, 1.0)
+			bg_color = Color.BLACK
+	%Segments.material.set_shader_parameter("color", color)
+	%Segments.material.set_shader_parameter("bg_color", bg_color)
+	data.color_index = color_index
