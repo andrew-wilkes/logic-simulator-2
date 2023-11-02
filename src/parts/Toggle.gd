df9e09af458eab258d2889eb8f6ae94717e54b98
andrew-wilkes <github@urgentclick.com>
2023-11-02 11:13:25 +0900

    Add toggle part

---
+++ src/parts/Toggle.gd
@@ -0,0 +1,21 @@
+class_name Toggle
+
+extends Part
+
+var race_counter_reset_counter = 0
+
+func _init():
+	category = UTILITY
+	order = 0
+
+
+func output_clock(level):
+	race_counter_reset_counter += 1
+	if race_counter_reset_counter > 3:
+		race_counter_reset_counter = 0
+		controller.reset_race_counters()
+	update_output_level(RIGHT, OUT, level)
+
+
+func _on_button_toggled(button_pressed):
+	output_clock(button_pressed)
