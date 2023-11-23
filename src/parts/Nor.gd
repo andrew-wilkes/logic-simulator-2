1bc3f6f379bedb4acd08a4e3f9e0675a906a5a1d
andrew-wilkes <github@urgentclick.com>
2023-11-23 20:40:35 +0900

    Add Not gate part

---
+++ src/parts/Nor.gd
@@ -0,0 +1,13 @@
+class_name NOR
+
+extends Part
+
+func _init():
+	pins = { [0, 0]: false, [0, 1]: false }
+	order = 700
+
+
+func evaluate_output_level(side, _port, level):
+	if side == LEFT:
+		level = pins[[side, A]] or pins[[side, B]]
+		update_output_level(RIGHT, OUT, not level)
--- src/parts/ToggleSwitch.gd
+++ src/parts/ToggleSwitch.gd
@@ -1,4 +1,4 @@
-class_name Toggle
+class_name ToggleSwitch
 
 extends Part
 
