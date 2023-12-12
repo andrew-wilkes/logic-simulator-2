5ef7c5ca4cb8474319574aed47c318db39129f53
andrew-wilkes <github@urgentclick.com>
2023-12-12 18:01:08 +0900

    Consolidate memory part functions

--- src/parts/RAM.gd
+++ src/parts/RAM.gd
@@ -2,9 +2,6 @@ class_name RAM
 
 extends BaseMemory
 
-var max_value = 0
-var mem_size = 0
-
 func _init():
 	order = 80
 	category = SYNC
@@ -28,38 +25,6 @@ func update():
 func show_bits():
 		%Bits.text = str(data.bits)
 		$Size.text = data.size
-
-
-func _on_size_text_submitted(new_text):
-	if new_text.length() > 0:
-		if new_text.is_valid_int() or new_text.right(1) in ["K", "M"]\
-			and new_text.left(-1).is_valid_int():
-				data.size = new_text
-				mem_size = get_mem_size(data.size)
-
-
-func _on_bits_text_submitted(new_text):
-	if new_text.is_valid_int():
-		data.bits = clampi(int(new_text), 1, 1024)
-		set_max_value()
-	elif show_display:
-		%Bits.text = ""
-
-
-func set_max_value():
-	max_value = int(pow(2, data.bits) - 1)
-
-
-func get_mem_size(dsize: String) -> int:
-	var n = 0
-	if dsize.is_valid_int():
-		n = int(dsize)
-	else:
-		n = int(dsize.left(-1))
-		if dsize.right(1) == "M":
-			n *= 1024
-		n *= 1024
-	return n
 
 
 func evaluate_output_level(side, port, _level):
@@ -99,11 +64,6 @@ func show_data(value):
 func show_address(address: int):
 	if show_display:
 		%Address.text = get_display_hex_value(address)
-
-
-func resize_memory(num_bytes):
-	values.resize(num_bytes)
-	values.fill(0)
 
 
 func reset():
--- src/parts/ROM.gd
+++ src/parts/ROM.gd
@@ -2,8 +2,6 @@ class_name ROM
 
 extends BaseMemory
 
-var max_value = 0
-var mem_size = 0
 var old_address = 0
 
 func _init():
@@ -28,38 +26,6 @@ func setup_instance():
 		load_data(data.file, false)
 
 
-func _on_size_text_submitted(new_text):
-	if new_text.length() > 0:
-		if new_text.is_valid_int() or new_text.right(1) in ["K", "M"]\
-			and new_text.left(-1).is_valid_int():
-				data.size = new_text
-				mem_size = get_mem_size(data.size)
-
-
-func _on_bits_text_submitted(new_text):
-	if new_text.is_valid_int():
-		data.bits = clampi(int(new_text), 1, 1024)
-		set_max_value()
-	elif show_display:
-		%Bits.text = ""
-
-
-func set_max_value():
-	max_value = int(pow(2, data.bits) - 1)
-
-
-func get_mem_size(dsize: String) -> int:
-	var n = 0
-	if dsize.is_valid_int():
-		n = int(dsize)
-	else:
-		n = int(dsize.left(-1))
-		if dsize.right(1) == "M":
-			n *= 1024
-		n *= 1024
-	return n
-
-
 func evaluate_bus_output_value(side, port, _value):
 	if side == LEFT and port == 0: # Change of address
 		set_output_data()
@@ -71,11 +37,6 @@ func set_output_data():
 		%Address.text = get_display_hex_value(address)
 		%Data.text = get_display_hex_value(values[address])
 	update_output_value(RIGHT, OUT, values[address])
-
-
-func resize_memory(num_bytes):
-	values.resize(num_bytes)
-	values.fill(0)
 
 
 func open_file():
