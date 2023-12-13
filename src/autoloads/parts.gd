1d42f2ba4871b87e497d1a6e9478c8b08ae3c521
andrew-wilkes <github@urgentclick.com>
2023-12-13 12:35:45 +0900

    Make parts discovery work in export build

--- src/autoloads/parts.gd
+++ src/autoloads/parts.gd
@@ -9,14 +9,14 @@ var scripts = {}
 func _init():
 	var path = "res://parts/"
 	var dir = DirAccess.open(path)
-	var files = Array(dir.get_files()).filter(func(fn): return fn.ends_with("scn"))
+	var files = Array(dir.get_files()).filter(func(fn): return fn.ends_with("gd"))
 	for file_name in files:
 		var part_name = file_name.get_file().get_slice('.', 0)
 		# Don't add the Block part as a Part option in the menu.
 		if part_name not in ["Block"]:
 			names.append(part_name)
-		scenes[part_name] = load(path + file_name)
-		scripts[part_name] = load(path + file_name.get_file().get_slice('.', 0) + ".gd")
+		scenes[part_name] = ResourceLoader.load(path + file_name.replace(".gd", ".tscn"))
+		scripts[part_name] = ResourceLoader.load(path + file_name)
 
 
 # This is used with circuit blocks where there are no visual elements internally.
