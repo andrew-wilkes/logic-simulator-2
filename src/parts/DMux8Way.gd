511928591b777a92fefbd5ccf407401c7bd5d6c5
andrew-wilkes <github@urgentclick.com>
2023-12-26 15:02:25 +0900

    Reverse input bit order of DMux parts to match IO part

--- src/parts/DMux8Way.gd
+++ src/parts/DMux8Way.gd
@@ -12,7 +12,7 @@ func evaluate_output_level(side, _port, _level):
 		var sel = 0
 		for n in 3:
 			sel *= 2
-			sel += int(pins.get([side, n + 1], false))
+			sel += int(pins.get([side, 3 - n], false))
 			update_outputs(sel)
 
 
@@ -21,6 +21,6 @@ func evaluate_bus_output_value(_side, _port, value):
 
 
 func update_outputs(sel):
-	var input = pins.get([LEFT, 0], 0)
+	var input = pins.get([LEFT, 0], false)
 	for n in 8:
 		update_output_level(RIGHT, n, input if sel == n else false)
