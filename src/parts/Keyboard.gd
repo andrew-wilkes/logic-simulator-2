608644f2b0f5a747e1676b777ec1cb8e0bbb0c3f
andrew-wilkes <github@urgentclick.com>
2024-04-27 14:41:47 +0900

    Delete redundant code from Keyboard part.

--- src/parts/Keyboard.gd
+++ src/parts/Keyboard.gd
@@ -2,7 +2,6 @@ class_name Keyboard
 
 extends Part
 
-var last_text_length = 0
 var show_placeholder = false
 var value_output: CircuitInput
 
@@ -20,18 +19,6 @@ func setup():
 	value_output.part = self
 	value_output.port = OUT
 	value_output.is_bus = true
-
-
-func _on_chars_text_changed(new_text):
-	var length = new_text.length()
-	if length > last_text_length:
-		# Output the character code of the last key that was typed by the user
-		var char_code = new_text.right(1).to_ascii_buffer()[0]
-		if DEBUG:
-			print(char_code)
-		value_output.value = char_code
-		controller.inject_circuit_input(value_output)
-	last_text_length = length
 
 
 func _on_blink_timer_timeout():
