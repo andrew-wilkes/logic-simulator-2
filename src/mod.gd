31d09caff267a809a9deb658a15ea39458615932
andrew-wilkes <github@urgentclick.com>
2024-05-12 15:52:03 +0900

    Add export preset for mods

--- src/mod.gd
+++ src/mod.gd
@@ -1,4 +1,4 @@
 extends Resource
 
 # Add entries for all scenes such as Parts and Tools which are in the Mod file
-@export var scenes: Array[String] = ["res://mods/Demo.tscn"]
+@export var scenes: Array[String] = ["res://mods/6530RRIOT.tscn", "res://mods/scenes/6530_fuses.tscn"]
